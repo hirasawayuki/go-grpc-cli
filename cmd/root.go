32f017ac84e869da305d57d117340bd7719ac72c
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-01-23 14:24:41 +0900

    fix root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -13,16 +13,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-grpc-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "gRPC app in Go",
+	Long:  `gRPC application written in Go.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
